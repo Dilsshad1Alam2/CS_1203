369f50028321841e3ff755b4af8823c56d9770b6
Dilsshad1Alam2 <dilshad.alam3215@gmail.com>
2022-10-21 23:29:18 +0530

    malloc

---
+++ malloc_implementation.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int i, n;
+
+    printf("enter the number of elements  ");
+    scanf("%d", &n);
+
+    int *ptr = (int *) malloc(n * sizeof(int));
+
+    if(ptr == NULL){
+        printf("out of memory");
+        exit(1);
+    }
+
+    for ( i = 0; i < n; i++)
+    {
+        printf("Enter element");
+        scanf("%d", ptr + i);
+    };
+
+    for ( i = 0; i < n; i++)
+    {
+        printf("%d  ", *(ptr + i) );
+    };
+
+    return 0;
+
+    
+
+}
